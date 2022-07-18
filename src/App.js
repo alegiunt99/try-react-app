60979f3f5ee78a633991fe3ba2362136fcccdf5d
Alessandro Giunta <alegiunt99@gmail.com>
2022-07-18 14:27:17 +0100

    Init + add Header and footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+// import{ Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      {/* HEADER */}
+      <header>
+
+        <Header />
+        
       </header>
+
+      <div className='content'>
+
+        {/* content... */}
+
+      </div>
+
+      {/* FOOTER */}
+      <div className='footer'>
+
+        <div className='container'>
+              <Footer />
+        </div>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+const Header = () => {
+  return (
+    <div className="App-header">
+        
+        <div className='container'>
+
+          <p>
+            Ciaone, la mia prima app react
+          </p>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
